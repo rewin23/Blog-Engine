5bcc3c6aff57894141ace0b0f49f8829f6206e28
Erwin Navarrete <enavarrete2010@gmail.com>
2018-02-01 19:28:05 -0300

    add form for session and modify session controller

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,7 +1,8 @@
 defmodule BlogEngine.SessionController do
   use BlogEngine.Web, :controller
+  alias BlogEngine.User
 
-	def new(conn, _params) do
-		render conn, "new.html"
-	end
+  def new(conn, _params) do
+    render conn, "new.html", changeset: User.changeset(%User{})
+  end
 end
