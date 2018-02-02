8b8dd78171a0b841c066f6c56de8598864c65eab
Erwin Navarrete <enavarrete2010@gmail.com>
2018-02-02 17:02:01 -0300

    add relations in models user/post

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,6 +6,7 @@ defmodule BlogEngine.User do
     field :username, :string
     field :email, :string
     field :password_digest, :string
+    has_many :posts, BlogEngine.Post
 
     #virtual Fields
     field :password, :string, virtual: true
