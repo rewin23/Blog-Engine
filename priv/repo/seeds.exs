11666f931005510c117fc6a337a2c17aeccff0ec
Erwin Navarrete <enavarrete2010@gmail.com>
2018-02-03 01:12:19 -0300

    add admin seeder

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias BlogEngine.Repo
+alias BlogEngine.Role
+alias BlogEngine.User
+role = %Role{}
+  |> Role.changeset(%{name: "Admin Role", admin: true})
+  |> Repo.insert!
+admin = %User{}
+  |> User.changeset(%{username: "admin", email: "admin@test.com", password: "test", password_confirmation: "test", role_id: role.id})
+  |> Repo.insert!
