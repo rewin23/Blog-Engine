f23ae081f763f42c8bd3dcd43ad8e23b2933ab5e
Erwin Navarrete <enavarrete2010@gmail.com>
2018-02-03 00:56:04 -0300

    create roles for model

---
+++ web/models/role.ex
@@ -0,0 +1,19 @@
+defmodule BlogEngine.Role do
+  use BlogEngine.Web, :model
+
+  schema "roles" do
+    field :name, :string
+    field :admin, :boolean, default: false
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :admin])
+    |> validate_required([:name, :admin])
+  end
+end
