46b2833dd2d6d727a5ddd341fc23d95848954992
Erwin Navarrete <enavarrete2010@gmail.com>
2018-02-06 19:04:32 -0300

    add show and index for show post in /

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule BlogEngine.PageController do
   use BlogEngine.Web, :controller
 
+  alias BlogEngine.Post
+
   def index(conn, _params) do
-    render conn, "index.html"
+  	posts = Repo.all(Post)
+    render(conn, "index.html", posts: posts)
+  end
+
+  def show(conn, %{"id" => id}) do
+    post = Repo.get!(Post, id)
+    render(conn, "show.html", post: post)
   end
 end
